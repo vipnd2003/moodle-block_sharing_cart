3db31b4feaeab7f990828059ec5bbfdd59f91b83
malu-ver2 <t-maruyama@ver2.jp>
2013-03-28 20:50:17 +0900

    Removed unnecessary settings.php and related strings

--- lang/en/block_sharing_cart.php
+++ lang/en/block_sharing_cart.php
@@ -1,6 +1,9 @@
 <?php
 
 $string['pluginname'] = 'Sharing Cart';
+$string['sharing_cart'] = $string['pluginname'];
+$string['sharing_cart_help'] = file_get_contents(__DIR__.'/help/sharing_cart.html');
+$string['sharing_cart:addinstance'] = 'Add a new Sharing Cart block';
 
 $string['backup'] = 'Copy to Sharing Cart';
 $string['restore'] = 'Copy to course';
@@ -10,15 +13,9 @@ $string['notarget'] = 'Target not found';
 $string['clipboard'] = 'Copying this shared item';
 $string['bulkdelete'] = 'Bulk delete';
 $string['confirm_backup'] = 'Are you sure you want to copy to Sharing Cart?';
-$string['confirm_restore'] = 'Are you sure you want to copy to course?';
 $string['confirm_delete'] = 'Are you sure you want to delete?';
 $string['confirm_delete_selected'] = 'Are you sure you want to delete all selected items?';
 $string['download'] = 'Download';
-
-$string['conf_plugins_heading'] = 'Plugin settings';
-$string['conf_plugins_nothing'] = 'There\'s no plugins to configure.';
-$string['conf_plugins_enabled_head'] = 'Enabled plugins';
-$string['conf_plugins_enabled_desc'] = 'Select plugins you want to use. Click with CTRL to select multiple item.';
 
 $string['err:record_id'] = 'Shared item ID was incorrect (not found)';
 $string['err:course_id'] = 'Course ID was incorrect (not found)';
@@ -32,8 +29,3 @@ $string['err:delete'] = 'An error occurred while deleting shared item';
 $string['err:record'] = 'An error occurred while operating data record';
 $string['err:tempdir'] = 'An error occurred while creating temporary directory';
 $string['err:cleanup'] = 'An error occurred while cleaning up temporary data';
-
-$string['err:not_implemented'] = 'Specified module does not support backup function';
-
-$string['sharing_cart'] = $string['pluginname'];
-$string['sharing_cart_help'] = file_get_contents(__DIR__.'/help/sharing_cart.html');
--- lang/ja/block_sharing_cart.php
+++ lang/ja/block_sharing_cart.php
@@ -1,6 +1,9 @@
 <?php
 
 $string['pluginname'] = '共有カート';
+$string['sharing_cart'] = $string['pluginname'];
+$string['sharing_cart_help'] = file_get_contents(__DIR__.'/help/sharing_cart.html');
+$string['sharing_cart:addinstance'] = '新しい共有カートブロックを追加する';
 
 $string['backup'] = '共有カートへコピー';
 $string['restore'] = 'コースへコピー';
@@ -10,15 +13,9 @@ $string['notarget'] = 'ターゲットが見つかりません';
 $string['clipboard'] = 'この共有アイテムをコピーする';
 $string['bulkdelete'] = '一括削除';
 $string['confirm_backup'] = '共有カートにコピーしますか？';
-$string['confirm_restore'] = 'コースにコピーしますか？';
 $string['confirm_delete'] = '削除してよろしいですか？';
 $string['confirm_delete_selected'] = '選択したアイテムを全て削除してもよろしいですか？';
 $string['download'] = 'ダウンロード';
-
-$string['conf_plugins_heading'] = 'プラグイン設定';
-$string['conf_plugins_nothing'] = 'プラグインがインストールされていないので設定項目はありません。';
-$string['conf_plugins_enabled_head'] = '有効にするプラグイン';
-$string['conf_plugins_enabled_desc'] = '有効にするプラグインを選択します。Ctrlキーを押しながらクリックすることで複数選択できます。';
 
 $string['err:record_id'] = '不正な共有アイテムIDです';
 $string['err:course_id'] = '不正なコースIDです';
@@ -32,8 +29,3 @@ $string['err:delete'] = '共有アイテムの削除に失敗しました';
 $string['err:record'] = 'DBアクセス時にエラーが発生しました';
 $string['err:tempdir'] = '一時ディレクトリの作成に失敗しました';
 $string['err:cleanup'] = 'クリーンアップ時にエラーが発生しました';
-
-$string['err:not_implemented'] = 'バックアップをサポートしていないモジュールです';
-
-$string['sharing_cart'] = $string['pluginname'];
-$string['sharing_cart_help'] = file_get_contents(__DIR__.'/help/sharing_cart.html');
